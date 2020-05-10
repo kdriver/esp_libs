297346bae004cb1bf5298ea093820cea87ba0f89
Keith <keith@just-the-one.co.uk>
2020-05-10 15:31:02 +0100

    More inits

---
+++ string_handler/String_handler.cc
@@ -0,0 +1,10 @@
+#include <StringHandler.h>
+
+void StringHandler::StringHandler(char *text)
+{
+
+}
+void StringHandler::~StringHandler()
+{
+
+}
---
+++ string_handler/String_handler.h
@@ -0,0 +1,11 @@
+#ifndef StringHandler_h
+#define StringHandler_h
+
+
+class StringHandler {
+    public:
+        StringHandler(char *s);
+        ~StringHandler();
+    private:
+};
+#endif
